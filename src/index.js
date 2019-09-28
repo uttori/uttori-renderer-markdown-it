45352fabebbdc05f1217d367920e11477c106f02
Matthew Callis <matthew.callis@gmail.com>
2019-09-28 16:17:22 -0700

    Updates to match plugin spec

--- src/index.js
+++ src/index.js
@@ -1,24 +1,94 @@
+const debug = require('debug')('Uttori.Plugin.Render.MarkdownIt');
 const MarkdownIt = require('markdown-it');
 const slugify = require('slugify');
 
 class MarkdownItRenderer {
-  constructor(config = {}) {
-    this.md = new MarkdownIt(config);
+  static register(context) {
+    if (!context || !context.hooks || typeof context.hooks.on !== 'function') {
+      throw new Error("Missing event dispatcher in 'context.hooks.on(event, callback)' format.");
+    }
+    context.hooks.on('render-content', MarkdownItRenderer.renderContent); // UttoriWiki
+    context.hooks.on('render-meta-description', MarkdownItRenderer.renderContent); // UttoriWiki
+    context.hooks.on('render-search-results', MarkdownItRenderer.renderCollection); // UttoriWiki
+    context.hooks.on('validate-config', MarkdownItRenderer.validateConfig); // UttoriWiki
   }
 
-  render(input) {
-    input = input || '';
+  static defaultConfig() {
+    return {
+      // Enable HTML tags in source
+      html: false,
+
+      // Use '/' to close single tags (<br />).
+      xhtmlOut: false,
+
+      // Convert '\n' in paragraphs into <br>, this is only for full CommonMark compatibility.
+      breaks: false,
+
+      // CSS language prefix for fenced blocks. Can be useful for external highlighters.
+      langPrefix: 'language-',
+
+      // Autoconvert URL-like text to links.
+      linkify: false,
+
+      // Enable some language-neutral replacement + quotes beautification.
+      typographer: false,
+
+      // Double + single quotes replacement pairs, when typographer enabled, and smartquotes on. Could be either a String or an Array.
+      // For example, you can use '«»„“' for Russian, '„“‚‘' for German, and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+      quotes: '“”‘’',
+
+      // Highlighter function. Should return escaped HTML, or '' if the source string is not changed and should be escaped externally.
+      // If result starts with <pre... internal wrapper is skipped.
+      // highlight: (/* str, lang */) => '',
+    };
+  }
+
+  static validateConfig(config, _context) {
+    debug('Validating config...');
+    if (!config || !config.renderMarkdownIt) {
+      debug('Config Warning: `renderMarkdownIt` configuration key is missing.');
+    }
+    debug('Validated config.');
+  }
+
+  static renderContent(content, context = { config: { renderMarkdownIt: {} } }) {
+    debug('Rendering content');
+    const config = { ...MarkdownItRenderer.defaultConfig(), ...context.config.renderMarkdownIt };
+    return MarkdownItRenderer.render(content, config);
+  }
+
+  static renderCollection(collection, context = { config: { renderMarkdownIt: {} } }) {
+    debug('Rendering collection:', collection.length);
+    const config = { ...MarkdownItRenderer.defaultConfig(), ...context.config.renderMarkdownIt };
+    return collection.map((document) => {
+      const html = MarkdownItRenderer.render(document.html, config);
+      return { ...document, html };
+    });
+  }
+
+  static render(content, config) {
+    if (!content) {
+      debug('No input provided, returning a blank string.');
+      return '';
+    }
+
+    const md = new MarkdownIt(config);
+
     // Remove empty links.
-    input = input.replace('[]()', '');
-    const missingLinks = input.match(/\[(.*)]\(\s?\)/g) || [];
+    content = content.replace('[]()', '');
+
+    // Find missing links, and link them.
+    const missingLinks = content.match(/\[(.*)]\(\s?\)/g) || [];
     if (missingLinks && missingLinks.length > 0) {
+      debug('Found missing links:', missingLinks.length);
       missingLinks.forEach((match) => {
-        const title = match.substr(1).slice(0, -3);
+        const title = match.slice(1).slice(0, -3);
         const slug = slugify(title, { lower: true });
-        input = input.replace(match, `[${title}](/${slug})`);
+        content = content.replace(match, `[${title}](/${slug})`);
       });
     }
-    return this.md.render(input).trim();
+
+    return md.render(content).trim();
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,57 @@
 const test = require('ava');
 const MarkdownItRenderer = require('../src');
 
-test('Renderer: MarkdownIt: render(): can initilize without a config', (t) => {
-  const renderer = new MarkdownItRenderer();
-  t.is(renderer.render('![test](/test.png)'), '<p><img src="/test.png" alt="test"></p>');
+test('MarkdownItRenderer.register(uttori): can register', (t) => {
+  t.notThrows(() => {
+    MarkdownItRenderer.register({ hooks: { on: () => {} } });
+  });
 });
 
-test('Renderer: MarkdownIt: render(): can accept a config', (t) => {
-  const renderer = new MarkdownItRenderer({ xhtmlOut: true });
-  t.is(renderer.render('![test](/test.png)'), '<p><img src="/test.png" alt="test" /></p>');
+test('MarkdownItRenderer.register(uttori): errors without event dispatcher', (t) => {
+  t.throws(() => {
+    MarkdownItRenderer.register({ hooks: {} });
+  }, 'Missing event dispatcher in \'context.hooks.on(event, callback)\' format.');
 });
 
-test('Renderer: MarkdownIt: render(): handles empty values', (t) => {
-  const renderer = new MarkdownItRenderer();
-  t.is(renderer.render(''), '');
-  t.is(renderer.render(' '), '');
-  t.is(renderer.render(null), '');
-  t.is(renderer.render(NaN), '');
-  t.is(renderer.render(undefined), '');
-  t.is(renderer.render(false), '');
-  t.is(renderer.render(), '');
+test('MarkdownItRenderer.defaultConfig(): can return a default config', (t) => {
+  t.notThrows(MarkdownItRenderer.defaultConfig);
 });
 
-test('Renderer: MarkdownIt: render(): replaces missing links with a slugified link', (t) => {
-  const renderer = new MarkdownItRenderer();
-  t.is(renderer.render('[Test]'), '<p>[Test]</p>');
-  t.is(renderer.render('[Test]()'), '<p><a href="/test">Test</a></p>');
-  t.is(renderer.render('[CrAzY CaSe SpAcEd]()'), '<p><a href="/crazy-case-spaced">CrAzY CaSe SpAcEd</a></p>');
+test('MarkdownItRenderer.validateConfig(config, _uttori): can validate a config', (t) => {
+  t.notThrows(MarkdownItRenderer.validateConfig);
+  t.notThrows(() => {
+    MarkdownItRenderer.validateConfig({ renderMarkdownIt: {} });
+  });
+});
+
+test('MarkdownItRenderer.renderContent(content, context): can render without a config', (t) => {
+  t.is(MarkdownItRenderer.renderContent('![test](/test.png)'), '<p><img src="/test.png" alt="test"></p>');
+});
+
+test('MarkdownItRenderer.renderContent(content, context): can accept a config', (t) => {
+  t.is(MarkdownItRenderer.renderContent('![test](/test.png)', { config: { renderMarkdownIt: { xhtmlOut: true } } }), '<p><img src="/test.png" alt="test" /></p>');
+});
+
+test('MarkdownItRenderer.renderCollection(collection, context): can render without a config', (t) => {
+  t.deepEqual(MarkdownItRenderer.renderCollection([{ html: '![test](/test.png)' }]), [{ html: '<p><img src="/test.png" alt="test"></p>' }]);
+});
+
+test('MarkdownItRenderer.renderCollection(collection, context): can accept a config', (t) => {
+  t.deepEqual(MarkdownItRenderer.renderCollection([{ html: '![test](/test.png)' }], { config: { renderMarkdownIt: { xhtmlOut: true } } }), [{ html: '<p><img src="/test.png" alt="test" /></p>' }]);
+});
+
+test('MarkdownItRenderer.render(content, config): handles empty values', (t) => {
+  t.is(MarkdownItRenderer.render(''), '');
+  t.is(MarkdownItRenderer.render(' '), '');
+  t.is(MarkdownItRenderer.render(null), '');
+  t.is(MarkdownItRenderer.render(NaN), '');
+  t.is(MarkdownItRenderer.render(undefined), '');
+  t.is(MarkdownItRenderer.render(false), '');
+  t.is(MarkdownItRenderer.render(), '');
+});
+
+test('MarkdownItRenderer.render(content, config): replaces missing links with a slugified link', (t) => {
+  t.is(MarkdownItRenderer.render('[Test]'), '<p>[Test]</p>');
+  t.is(MarkdownItRenderer.render('[Test]()'), '<p><a href="/test">Test</a></p>');
+  t.is(MarkdownItRenderer.render('[CrAzY CaSe SpAcEd]()'), '<p><a href="/crazy-case-spaced">CrAzY CaSe SpAcEd</a></p>');
 });
